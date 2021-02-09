6bb91f1199dcab1be8e65e01c134b7b00dd530b2
Pouya <devpouya@gmail.com>
2021-02-09 10:49:45 +0100

    printloss

--- src/h02_learn/train.py
+++ src/h02_learn/train.py
@@ -141,6 +141,7 @@ def train(trainloader, devloader, model, eval_batches, wait_iterations, optim_al
         for (text, pos), (heads, rels) in trainloader:
 
             loss = train_batch(text, pos, heads, rels, model, optimizer)
+            print(loss)
             train_info.new_batch(loss)
             if train_info.eval:
                 dev_results = evaluate(devloader, model)
