9327c929dde2c7974d50369d2fdcce377b432db1
Pouya <devpouya@gmail.com>
2021-03-24 14:52:25 +0100

    names no need names

--- src/h02_learn/algorithm/transition_parsers.py
+++ src/h02_learn/algorithm/transition_parsers.py
@@ -51,11 +51,6 @@ class ShiftReduceParser():
         (_, ind) = self.stack[-2]
         self.stack[-2] = (c, ind)
         return c
-    def subtree_rep_eager(self,top,second,rel_embed,linear,is_right=False):
-        if not is_right:
-            return self.subtree_rep(top,second,rel_embed,linear)
-        else:
-            return self.subtree_rep(second,top,rel_embed,linear)
 
     def subtree_rep_hybrid(self, top, second, rel_embed,linear):
 
@@ -72,7 +67,6 @@ class ShiftReduceParser():
     def shift(self):
         item = self.buffer.pop(0)
         self.stack.append(item)
-        self.action_history_names.append(constants.shift)
         return item[0]
 
     def reduce_l(self, rel,rel_embed,linear):
@@ -81,7 +75,6 @@ class ShiftReduceParser():
         #left = self.buffer[0]
         self.stack.pop(-2)
         self.arcs.append((top[1], second[1], rel))
-        self.action_history_names.append(constants.reduce_l)
         c = self.subtree_rep(top, second,rel_embed,linear)
         return c
 
@@ -89,7 +82,6 @@ class ShiftReduceParser():
         second = self.stack[-2]
         top = self.stack[-1]
         self.arcs.append((second[1], top[1], rel))
-        self.action_history_names.append(constants.reduce_r)
         self.stack.pop(-1)
         #self.buffer[0] = top
         c = self.subtree_rep(second, top,rel_embed,linear)
@@ -107,14 +99,12 @@ class ShiftReduceParser():
         # if not self.arcs.has_incoming(stack_top):
         self.stack.pop(-1)
         self.arcs.append((left[1], top[1],rel))
-        self.action_history_names.append(constants.left_arc_eager)
         c = self.subtree_rep_hybrid(left, top, rel_embed,linear)
         return c
     def left_arc_prime(self,rel,rel_embed,linear):
         top = self.stack[-1]
         second = self.stack[-2]
         self.arcs.append((top[1],second[1],rel))
-        self.action_history_names.append(constants.left_arc_prim)
         c = self.subtree_rep(top,second,rel_embed,linear)
         return c
 
@@ -145,29 +135,11 @@ class ShiftReduceParser():
         left = self.buffer[0]
         self.stack.append(left)
         self.arcs.append((top[1], left[1],rel))
-        self.action_history_names.append(constants.right_arc_eager)
         c = self.subtree_rep(top, left, rel_embed,linear)
         self.buffer.pop(0)
         return c
 
-    def left_arc_hybrid(self, act_emb,rel,rel_embed,linear):
-        stack_top = self.stack[-1]
-        buffer_first = self.buffer[0]
-        self.arcs.append((buffer_first[1], stack_top[1],rel))
-        self.action_history_names.append(constants.left_arc_hybrid)
-        c = self.subtree_rep(buffer_first, stack_top, rel_embed,linear)
-        self.stack.pop(-1)
 
-        return c
-
-    def reduce_r_hybrid(self, act_emb, rel, rel_embed, linear):
-        top = self.stack[-1]
-        second = self.stack[-2]
-        self.arcs.append((second[1],top[1],rel))
-        self.action_history_names.append(constants.reduce_r)
-        self.stack.pop(-1)
-        c = self.subtree_rep(second,top,rel_embed,linear)
-        return c
 
 
     def is_parse_complete(self, special_op=False):
