8222add109a9ef39ebc622499d77ed6e360ab107
Pouya <devpouya@gmail.com>
2021-05-05 21:43:34 +0200

    oldmh4 fixed de

--- src/h02_learn/dataset/syntax.py
+++ src/h02_learn/dataset/syntax.py
@@ -55,13 +55,13 @@ class DerivationDataset(Dataset):
         token_mapping2 = []
         for token in enc:
             tokenout = []
-            for ids in token:
-                tokenout.append(idx)
-                idx += 1
-
-            token_mapping.append(tokenout[0])
-            token_mapping.append(tokenout[-1])
-            token_mapping2.append(tokenout)
+            if len(token)>=1:
+                for ids in token:
+                    tokenout.append(idx)
+                    idx += 1
+                token_mapping.append(tokenout[0])
+                token_mapping.append(tokenout[-1])
+                token_mapping2.append(tokenout)
 
         return encoded['input_ids'].squeeze(0), torch.LongTensor(token_mapping).to(device=constants.device)
 
